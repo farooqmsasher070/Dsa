7cb0b98b7c22c55b1b144c3f752f5bb04288398e
farooqsmasher070 <iamfarooq1@outlook.com>
2023-12-05 11:33:44 +0000

    one d array with main method

---
+++ OneDArrar.java
@@ -0,0 +1,14 @@
+package Dsa;
+
+public class OneDArrar{
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("testing from git");
+    }
+
+
+    
+}
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 #Self Preparation
 
 testing git 
+
+
